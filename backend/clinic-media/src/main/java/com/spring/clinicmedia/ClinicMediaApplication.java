91a7e1f975de5a47da725ef2ea0b99db1e72c9e0
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-04-29 00:53:02 +0300

    create backend project

---
+++ backend/clinic-media/src/main/java/com/spring/clinicmedia/ClinicMediaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.clinicmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicMediaApplication.class, args);
+    }
+
+}
