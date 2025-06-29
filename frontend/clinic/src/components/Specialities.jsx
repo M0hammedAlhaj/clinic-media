1c79f383cd290225c793ddd7ae58d863965d47e2
MohammedAlhaj <mohamedalhaj900@gmail.com>
2025-06-29 06:52:26 +0300

    refact(responsizety)

--- frontend/clinic/src/components/Specialities.jsx
+++ frontend/clinic/src/components/Specialities.jsx
@@ -11,12 +11,19 @@ const Specialities = () => {
       className="flex flex-col items-center gap-8 px-6 py-6 text-center"
       dir="rtl"
     >
-      <h1 className="text-5xl font-bold">العيادات والتخصصات</h1>
-      <p className="text-lg text-gray-600">
+      <h1 className="text-4xl md:text-4xl lg:text-5xl font-bold text-center">
+        العيادات والتخصصات
+      </h1>
+      <p className="text-gray-500 px-3 text-[.9rem] md:text-[1rem] lg:text-[1.1rem] ">
         نقدم لك مجموعة من التخصصات الطبية لتلبية جميع احتياجاتك الصحية بكل
         احترافية.
       </p>
-      <ul className="grid grid-cols-3 md:grid-cols-5 gap-10">
+      <ul className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-10 gap-10">
+        <SpecialitiesItem title={"باطني"} image={SpecialitiesOne} />
+        <SpecialitiesItem title={"جراحة"} image={SpecialitiesTow} />
+        <SpecialitiesItem title={"عظام"} image={SpecialitiesThree} />
+        <SpecialitiesItem title={"قلب"} image={SpecialitiesFour} />
+        <SpecialitiesItem title={"أسنان"} image={SpecialitiesFive} />
         <SpecialitiesItem title={"باطني"} image={SpecialitiesOne} />
         <SpecialitiesItem title={"جراحة"} image={SpecialitiesTow} />
         <SpecialitiesItem title={"عظام"} image={SpecialitiesThree} />
--- frontend/clinic/src/components/SpecialitiesItem.jsx
+++ frontend/clinic/src/components/SpecialitiesItem.jsx
@@ -4,8 +4,8 @@ const SpecialitiesItem = ({ title, image }) => {
   return (
     <div>
       <a href="" className="flex flex-col items-center gap-2 text-center">
-        <img src={image} alt="" className="min-w-[5rem]"/>
-        <h3 className="text-[1rem] font-bold">{title}</h3>
+        <img src={image} alt="" className=" max-w-[3rem] md:max-w-[5rem]"/>
+        <h3 className="">{title}</h3>
       </a>
     </div>
   );
